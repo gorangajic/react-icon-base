8ee0c1e46c25deda6ba87104288c94ac8cc59691
Benjamin Tatum <bentatum@me.com>
2016-07-09 12:08:56 -0400

    issue(remove-default-color-prop): removed default color, added unit tests, standard and commitizen

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 import { default as React, PropTypes } from 'react'
-const defaultColor = '#444'
-const defaultSize = '1em'
 
-const IconBase = ({ children, color, size, style, ...props }, { reactIconBase }) => {
-    const computedSize = size ? size : (reactIconBase && reactIconBase.size || defaultSize)
-    const computedColor = color ? color : (reactIconBase && reactIconBase.color || defaultColor)
-    const computedStyle = {
+const IconBase = ({ children, color, size, style, ...props }, { reactIconBase = {} }) => {
+  const computedSize = size || reactIconBase.size || '1em'
+  return (
+    <svg
+      children={children}
+      fill='currentColor'
+      preserveAspectRatio='xMidYMid meet'
+      height={computedSize}
+      width={computedSize}
+      {...reactIconBase}
+      {...props}
+      style={{
         verticalAlign: 'middle',
-        color: computedColor,
-        ...(reactIconBase && reactIconBase.style || {}),
+        color: color || reactIconBase.color,
+        ...(reactIconBase.style || {}),
         ...style
-    }
-    return (
-        <svg
-            fill="currentColor"
-            preserveAspectRatio="xMidYMid meet"
-            height={computedSize}
-            width={computedSize}
-            {...reactIconBase}
-            {...props}
-            style={computedStyle}
-        >
-            {children}
-        </svg>
-    )
+      }}
+    />
+  )
 }
 
 IconBase.propTypes = {
-    color: PropTypes.string,
-    size: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number
-    ]),
-    style: PropTypes.object
+  color: PropTypes.string,
+  size: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  style: PropTypes.object
 }
 
 IconBase.contextTypes = {
-    reactIconBase: PropTypes.shape(IconBase.propTypes)
+  reactIconBase: PropTypes.shape(IconBase.propTypes)
 }
 
 export default IconBase
