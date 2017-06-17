ce3dfb34e5b9f8d957d366106e7b342abef0a9d1
Aaron O'Mullan <aaron.omullan@gmail.com>
2017-06-18 00:21:47 +0200

    Allow explicitly setting width & height
    Useful for non-square SVGs (e.g: a logo).

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { default as React } from 'react'
 import PropTypes from 'prop-types'
-const IconBase = ({ children, color, size, style, ...props }, { reactIconBase = {} }) => {
+const IconBase = ({ children, color, size, style, width, height, ...props }, { reactIconBase = {} }) => {
   const computedSize = size || reactIconBase.size || '1em'
   return (
     <svg
       children={children}
       fill='currentColor'
       preserveAspectRatio='xMidYMid meet'
-      height={computedSize}
-      width={computedSize}
+      height={height || computedSize}
+      width={width || computedSize}
       {...reactIconBase}
       {...props}
       style={{
@@ -24,6 +24,14 @@ const IconBase = ({ children, color, size, style, ...props }, { reactIconBase = 
 IconBase.propTypes = {
   color: PropTypes.string,
   size: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  width: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  height: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number
   ]),
